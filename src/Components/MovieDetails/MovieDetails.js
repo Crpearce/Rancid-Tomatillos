a51ac00c40677a903f7f10cdb85a5ad21343334a
Carissa Gross <carissamgross@gmail.com>
2022-10-23 13:19:13 -0600

    Finalize styling

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-// import ReactPlayer from "react-player";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Pagination, Navigation, Mousewheel, Keyboard } from "swiper";
 import dayjs from 'dayjs';
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
-// import "swiper/css/scrollbar";
 import "./MovieDetails.css";
 
 class MovieDetails extends Component {
@@ -16,30 +9,15 @@ class MovieDetails extends Component {
     this.state = {
       singleMovie: {},
       trailer: '',
-      error: "",
+      error: '',
     };
   }
 
   componentDidMount() {
-    fetch(
-      `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.singleMovieID}/videos`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Error receiving Data");
-        } else {
-          return response.json();
-        }
-      })
-      .then((data) => {
-        this.setState({ trailer: data.videos[0] })
-        console.log(this.state.trailer)
-      })
-      .catch((error) => this.setState({ error: error.message }));
-
+    
     fetch(
       `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.singleMovieID}`
-    )
+      )
       .then((response) => {
         if (!response.ok) {
           throw new Error("Error receiving Data");
@@ -49,38 +27,49 @@ class MovieDetails extends Component {
       })
       .then((response) => this.setState({ singleMovie: response.movie }))
       .catch((error) => this.setState({ error: error.message }));
-  }
 
+        fetch(
+          `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.singleMovieID}/videos`
+          )
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Error receiving Data");
+            } else {
+              return response.json();
+            }
+          })
+          .then((data) => {
+            this.setState({ trailer: data.videos[0] })
+          })
+          .catch((error) => this.setState({ error: error.message }));
+    }
 
   render() {
+    console.log(this.state.singleMovie)
       return (
         <div className="movie-details-container" key={this.state.singleMovie.id}>
+              <img className="image" src={this.state.singleMovie.backdrop_path} alt={this.state.singleMovie.title}/>
           <div className="movie-card-container">
             <section className='movie-details'>
-              <img className="image" src={this.state.singleMovie.backdrop_path} alt={this.state.singleMovie.title}/>
+              <p className="other-details"> <b>{dayjs(this.state.singleMovie.release_date).format("MM/DD/YYYY")}</b> | <b>{this.state.singleMovie.runtime} Minutes</b> | <b>Rating: {parseInt(this.state.singleMovie.average_rating)}</b></p>
               <h2 className="title">{this.state.singleMovie.title}</h2>
               <p className="overview">{this.state.singleMovie.overview}</p>
-              <p className="release-date"><b>{dayjs(this.state.singleMovie.release_date).format("MM/DD/YYYY")} | {this.state.singleMovie.genres}</b></p>
-              <p className="rating"><b>{parseInt(this.state.singleMovie.average_rating)} / 10</b> </p>
             </section>
           </div>
           <section className="movie-trailer-container">
-            {/* <div className='movie-trailer'> */}
               <iframe
               src={`https://www.youtube.com/embed/${this.state.trailer.key}`}
               frameBorder='0'
               title="Embedded youtube"
               width='80%'
               ></iframe>
-            {/* </div> */}
           </section>
           <Link to="/">
-            <button className="home-button">Home</button>
+            <button className="home-button">Rancid Tomatillos</button>
           </Link>
         </div>
       );
     }
   }
-// }
 
 export default MovieDetails;
